545bf3c38f929865f70fb6327eb770b619cd841e
Kesakoyt <158209859+Kesakoyt@users.noreply.github.com>
2026-01-11 14:04:53 +0100

    Add Firebase configuration and initialization

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0J27oBxUbZG4ucVJG8kPLuG21nP7mAck",
+    authDomain: "revise-61c6b.firebaseapp.com",
+    projectId: "revise-61c6b",
+    storageBucket: "revise-61c6b.firebasestorage.app",
+    messagingSenderId: "412050181768",
+    appId: "1:412050181768:web:7fffb7e0306c2d2b11d93d",
+    measurementId: "G-RTXCZMXX5Y"
+};
+
+// Initialisation globale
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.firestore();
+window.auth = firebase.auth();
+window.googleProvider = new firebase.auth.GoogleAuthProvider();
+
+console.log("Firebase connecté ✅");
